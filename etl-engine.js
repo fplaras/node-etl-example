d343ab5246756cb41acc7e767bf886016f395fd8
Frank Perez <1815587+fplaras@users.noreply.github.com>
2022-02-06 18:03:14 -0500

    initial version

---
+++ etl-engine.js
@@ -0,0 +1,149 @@
+/**
+ * Dependencies
+ */
+const fs = require('fs');
+const momentjs = require('moment-timezone');
+const service = require('./etl-service');
+const moment = require('moment');
+
+let executionFileName = '';
+
+/**
+ * Failure Callback
+ */
+function failureCallback(data) {
+    fs.appendFile('./output/' + executionFileName + "_error_log.txt", "Errors: " + data, (err) => {
+        //error writting to the error log file
+        if (err) console.log(err);
+    });
+}
+
+/**
+ * Get Datetime in Timezone
+ */
+function GetTimezoneDtttm() {
+    return momentjs.tz("America/New_York").format('Y-MM-DDTHH-mm-ss-SSSS');
+}
+
+/**
+ * Calculates datetime difference and returns duration
+ */
+function getTimeInterval(start, end) {
+    return moment.duration(end.diff(start)).asMilliseconds()
+}
+
+/**
+ * Write to Log File
+ */
+function logFile(filePath, data, interval) {
+    return new Promise(function (resolve, reject) {
+        if (interval) {
+            fs.appendFile('./output/' + filePath + '/' + filePath + '_log.txt', data + interval + 'ms\n', (err) => {
+                //error creating the error log file
+                if (err) reject('Error in readTargets: ' + err);
+                console.log(data + interval + 'ms');
+                resolve();
+            });
+        } else {
+            fs.appendFile('./output/' + filePath + '/' + filePath + '_log.txt', data + '\n', (err) => {
+                //error creating the error log file
+                if (err) reject('Error in readTargets: ' + err);
+                resolve();
+            });
+        }
+    });
+}
+
+/**
+ * Process Source File
+ */
+async function processSourceFolder(data) {
+    await logFile(data, 'Pipeline Step: Process Source File...');
+    let startTime = moment();
+    await service.cleanSourceRecords(data);
+    let endTime = moment();
+    await logFile(data, 'Pipeline Step: Process Source File...Completed Elapsed Time: ', getTimeInterval(startTime, endTime));
+}
+
+/**
+ * Read Targets
+ */
+async function readTargets(data) {
+    let startTime = moment();
+    startTotal = startTime;
+    await logFile(data, 'Pipeline Step: Read Targets...');
+    await service.readTargets(data);
+    let endTime = moment();
+    await logFile(data, 'Pipeline Step: Read Targets...Completed Elapsed Time: ', getTimeInterval(startTime, endTime));
+}
+
+
+/**
+ * Read Products
+ */
+async function readAndTransformProducts(data) {
+    let startTime = moment();
+    await logFile(data, 'Pipeline Step: Read Products...');
+    await service.readProductsAndTransform(data)
+        .then(async value => {
+            logFile(data, 'Products with Missing Data: ' + JSON.stringify(value, null, 2));
+        });
+    let endTime = moment();
+    await logFile(data, 'Pipeline Step: Read Products...Completed Elapsed Time: ', getTimeInterval(startTime, endTime));
+}
+
+/**
+ * Write Result File
+ */
+async function writeOutputFile(data) {
+    await logFile(data, 'Pipeline Step: Writting Output File...');
+    let startTime = moment();
+    await service.writeOutputFile(data);
+    let endTime = moment();
+    await logFile(data, 'Pipeline Step: Writting Output File...Completed Elapsed Time: ', getTimeInterval(startTime, endTime));
+    await logFile(data, 'Total Elapsed Time: ', getTimeInterval(startTotal, endTime));
+}
+
+/**
+ * Start Pipeline
+ */
+function startEtlPipeline() {
+    console.log('/***************************************************/');
+    console.log('/***************************************************/');
+    console.log('Pipeline Step: Initialize Output File');
+    return new Promise(function (resolve, reject) {
+        //define execution folder name
+        executionFileName = 'execution_' + GetTimezoneDtttm();
+        //try catch for creating folder directory
+        try {
+            //if folder does not exist create folder
+            if (!fs.existsSync('./output/' + executionFileName)) {
+                //create execution folder
+                fs.mkdirSync('./output/' + executionFileName);
+                //create execution log file
+                fs.appendFile('./output/' + executionFileName + '/' + executionFileName + "_log.txt", "Pipeline Step: Initialize Output File\n", (initializeLogFileError) => {
+                    //error creating the execution log file
+                    if (initializeLogFileError) {
+                        //stop execution and log error
+                        reject(initializeLogFileError);
+                    } else {
+                        //resolve and continue with execution file name created
+                        resolve(executionFileName);
+                    }
+                });
+            }
+        } catch (createFolderError) {
+            //catch exception creating directory and log
+            reject(createFolderError);
+        }
+    });
+}
+
+startEtlPipeline()
+    .then(async function (result) {
+        await processSourceFolder(result);
+        await readTargets(result);
+        await readAndTransformProducts(result);
+        await writeOutputFile(result);
+    })
+    .catch(failureCallback);
---
+++ etl-service.js
@@ -0,0 +1,246 @@
+/**
+ * Dependencies
+ */
+ const csvParser = require('papaparse');
+ const fs = require('fs');
+
+let currentTargets = [];
+let transformedData = [];
+let missingValues = [];
+let hasMissingValues = false;
+
+module.exports = {
+    readProductsAndTransform,
+    readTargets,
+    writeOutputFile,
+    cleanSourceRecords
+};
+
+/**
+ * Process Source Folder
+ * sku id 11215090 presented some formatting issues
+ */
+ function cleanSourceRecords(excecutionFileName){
+    return new Promise(function(resolve,reject){
+        let content = fs.readFileSync('source/products.txt', "utf8");
+        const headers = content.slice(0, content.indexOf("\n")).split("|");
+        const rows = content.slice(content.indexOf("\n") + 1).split("\n");
+        let newRows = [];
+        rows.forEach(element => {
+             
+            newRows.push(element.replace(/(^"|"$)|\"/g, ''));
+        });
+        //let count = 0;
+        var newFile = fs.createWriteStream('output/' + excecutionFileName + '/products.txt');
+        newFile.write(headers.join('|' ));
+        newRows.forEach(element => {
+            newFile.write(element);
+           // count++;
+        });
+        newFile.end();
+        resolve(newFile);
+    });
+  }
+
+  /**
+   * Write Output File
+   */
+   function writeOutputFile(excecutionFileName) {
+    return new Promise(function (resolve, reject) {
+        try {
+            var content = JSON.stringify(transformedData, null, 2)
+            fs.writeFile('output/' + excecutionFileName + '/result.txt', content, 'utf8', err => {
+                if (err) {
+                    console.error(err)
+                    reject(err);
+                } else {
+                    resolve(excecutionFileName);
+                }
+            });
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+/*******************
+ * Parser Functions
+ * ******************/
+
+/**
+ * Configurations
+ */
+
+//Target Configuration
+ const targetsConfig = {
+    header: false,
+    quoteChar: '\'',
+    delimiter: "\t",
+    step: targetStep,
+    escapeChar: "\'",
+    skipEmptyLines: true,
+};
+
+//Products Configuration
+const productsConfig = {
+    header: true,
+    //preview: 10, //limit results
+    delimiter: "|",
+    step: productsStep,
+    transform: transformProducts,
+    escapeChar: "\"",
+    skipEmptyLines: true,
+};
+
+/**
+ * Functions
+ */
+//Process one target record at a time
+function targetStep(row, parser){
+    currentTargets.push({ pKey: row.data[0], fKey: row.data[1] });
+}
+
+//Parse Targets
+function readTargets(excecutionFileName) {
+    console.log('Service: Reading Targets...');
+    return new Promise(function (resolve, reject) {
+        try {
+            let content = fs.readFileSync('target/targets.txt', "utf8");
+            csvParser.parse(content, targetsConfig);
+            resolve(excecutionFileName);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+//Parse Products
+function readProductsAndTransform(excecutionFileName) {
+    console.log('Service: Reading Products...');
+    return new Promise(function (resolve, reject) {
+        try {
+            let content = fs.readFileSync('output/'+ excecutionFileName +'/products.txt', "utf8");
+            csvParser.parse(content, productsConfig);
+            resolve(missingValues);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+//Process one product record at a time
+function productsStep(results, parser) {
+    let transformedRecord = {
+        "sku": results.data.sku,
+        "name": results.data.name,
+        "description": results.data.description,
+        "priceList": parseFloat(results.data.price),
+        "taxonomy": [
+            results.data.store_id,
+            results.data.department_id,
+            results.data.category_id,
+            results.data.subcategory_id
+        ],
+        "targetValues": []
+    };
+
+    transformedData.push(populateTargetValues(transformedRecord, results));
+
+    //if missing value flag
+    if(hasMissingValues){
+        //reset flag
+        hasMissingValues = false;
+        //add result to array
+        missingValues.push(transformedRecord);
+    }
+}
+
+//Map transformed to product to targets and return update object
+function populateTargetValues(transformedRecord, row) {
+
+    let store_idTarget = currentTargets.find(obj => obj.pKey === row.data.store_id);
+    if (store_idTarget)
+        transformedRecord.targetValues.push(store_idTarget.fKey);
+    else
+        transformedRecord.targetValues.push('not_available');
+
+    let department_idTarget = currentTargets.find(obj => obj.pKey === row.data.department_id);
+    if (department_idTarget)
+        transformedRecord.targetValues.push(department_idTarget.fKey);
+    else
+        transformedRecord.targetValues.push('not_available');
+
+    let category_idTarget = currentTargets.find(obj => obj.pKey === row.data.category_id);
+    if (category_idTarget)
+        transformedRecord.targetValues.push(category_idTarget.fKey);
+    else
+        transformedRecord.targetValues.push('not_available');
+
+    let subcategory_idTarget = currentTargets.find(obj => obj.pKey === row.data.subcategory_id);
+    if (subcategory_idTarget)
+        transformedRecord.targetValues.push(subcategory_idTarget.fKey);
+    else
+        transformedRecord.targetValues.push('not_available');
+
+    return transformedRecord;
+}
+
+// Transform each data point
+function transformProducts(data, header) {
+    switch (header) {
+        case 'store_id':
+            if (parseInt(data)) {
+                return 'sto' + data;
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        case 'department_id':
+            if (parseInt(data)) {
+                return 'dep' + data;
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        case 'category_id':
+            if (parseInt(data)) {
+                return 'cat' + data;
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        case 'subcategory_id':
+            if (parseInt(data)) {
+                return 'sub' + data;
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        case 'price':
+            {
+                var priceVal = data.replace('$', '');
+                if (parseFloat(priceVal)) {
+                    return priceVal;
+                } else {
+                    hasMissingValues = !hasMissingValues ? true : false;
+                    return 'not_available'
+                }
+            }
+        case 'name':
+            if (data && data !== 'not_available') {
+                return data.replace(/(^'|'$)|\'/g, '\"');
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        case 'description':
+            if (data && data !== 'not_available') {
+                return data;
+            } else {
+                hasMissingValues = !hasMissingValues ? true : false;
+                return 'not_available'
+            }
+        default:
+            return data;
+    }
+}
